ed9b5a982d94adc198094510cee2908ef3fac82f
appkenny <kzeng.95@gmail.com>
2021-10-13 00:07:13 -0700

    create dog model and controller EOD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_231324) do
+ActiveRecord::Schema.define(version: 2021_10_12_234556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_231324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "dog_id", null: false
+    t.string "color", null: false
   end
 
 end
